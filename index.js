8099047100b967b8809d80c2f1be7696f479ee54
Xuefeng Wu <sirowood@gmail.com>
2021-12-23 17:19:13 +0200

    step3 done

--- index.js
+++ index.js
@@ -30,6 +30,16 @@ app.get("/api/persons", (request, response) => {
     response.send(persons)
 })
 
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.send(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 app.get("/info", (request, response) => {
     let text = `<p>Phonebook has info for ${persons.length} people</p>`
     text += `<p>${Date()}</p>`
