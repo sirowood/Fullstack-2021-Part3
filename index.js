3f4d110395fb19698f9728487dbd193b663b23c3
Xuefeng Wu <sirowood@gmail.com>
2021-12-23 22:12:39 +0200

    change port

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.get("/info", (request, response) => {
     response.send(text)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
