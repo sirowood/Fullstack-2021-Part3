50ea038a1a111f58ce4b1cc750e1a1830a9e0d9d
Xuefeng Wu <sirowood@gmail.com>
2021-12-25 15:33:39 +0200

    3.19 done

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id'})
+    } else if (error.errors.name.kind === 'unique') {
+        return response.status(403).send({ error: error.errors.name.message}) 
     } else if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
     }
@@ -82,7 +84,7 @@ app.delete("/api/persons/:id", (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     const body = request.body
 
     const person = new Person({
@@ -90,9 +92,12 @@ app.post("/api/persons", (request, response) => {
         number: body.number
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person
+        .save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.get("/info", (request, response) => {
